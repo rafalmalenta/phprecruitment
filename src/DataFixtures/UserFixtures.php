8003f7723bef4b7177028b12e815a428863046c7
Raf <rmalenta@wp.pl>
2021-06-18 08:39:58 +0200

    added entities

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Hero;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordEncoder;
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder=$passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $USER = new User();
+        $USER->setUsername("random")
+            ->setPassword($this->passwordEncoder->hashPassword($USER, "1234"));
+
+        $manager->persist($USER);
+
+        $manager->flush();
+    }
+
+}
---
+++ src/Entity/BlogPost.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\BlogPostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BlogPostRepository::class)
+ */
+class BlogPost
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $fullContent;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private string $shortContent;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFullContent(): ?string
+    {
+        return $this->fullContent;
+    }
+
+    public function setFullContent(string $fullContent): self
+    {
+        $this->fullContent = $fullContent;
+
+        return $this;
+    }
+
+    public function getShortContent(): ?string
+    {
+        return $this->shortContent;
+    }
+
+    public function setShortContent(string $shortContent): self
+    {
+        $this->shortContent = $shortContent;
+
+        return $this;
+    }
+}
